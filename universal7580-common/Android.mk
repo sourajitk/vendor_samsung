3f250e5d74f2bf700c68c9878e4b0a8211dcb77f
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2020-04-21 15:42:07 -0400

    universal7580-common: Drop support for few devices.
    Dropped as I don't build for them.

--- universal7580-common/Android.mk
+++ universal7580-common/Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter a3xelte a5xelte a7xelte gvwifi gvlte j7elte s5neolte,$(TARGET_DEVICE)),)
+ifneq ($(filter a7xelte,$(TARGET_DEVICE)),)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := libGLES_mali
